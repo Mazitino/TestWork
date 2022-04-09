50ce4a821db440e2e5342c863d782be8ee3df31e
aydar.mazitov <aydar.mazitov@gmail.com>
2022-04-10 04:40:52 +0500

    Добавил еще один способ решение задачи 3

--- 03/index.js
+++ 03/index.js
@@ -3,9 +3,10 @@ function toHex(value) {
     if(value==0){
         return "0"
     }
-    
+
+    //Метод 1 
+    /*
     let list =[]
-    
     for(let i=7; i>=0; i--) //размер 4 байт FFFF FFFF
     {
         let val = (value >> (i*4)) & 0xf; //сдвиг до первого байта & FFFF
@@ -15,9 +16,36 @@ function toHex(value) {
             list.push(result)
         }
     }
+    */
+
+
+    //Метод 2 (перевод в 16-сиситему)
+    let b;
+    let c = [];
+    let map = new Map();
+    map.set(10, "a")
+        .set(11, "b")
+        .set(12, "c")
+        .set(13, "d")
+        .set(14, "e")
+        .set(15, "f");
+        
+    b = value;
+    c.push( value % 16);
+    do{
+    b = Math.trunc(b / 16);
+    c.push( b % 16);
+    }while(b>=16)
     
-    return list.join(''); //Объединяем все элементы в строку
+    
+    for (let d in c) {
+        (c[d]>9) ? (c[d]=map.get(c[d])) : ('')
+    }
+
+    c.reverse();
+    return c.join(''); 
 
 }
+
 
 module.exports = toHex;
